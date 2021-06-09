a03156437ead79984091b60a4c3343a8fa89f2e7
Shahid <shahisir@in.ibm.com>
2021-06-09 05:32:00 +0000

    edit distance dp

--- Dynamic Programming/7_lcs.cpp
+++ Dynamic Programming/7_lcs.cpp
@@ -1,9 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(string x, string y, int n, int m){
-    int dp[n+1][m+1];
+#define MAX 20
 
+int dp[MAX][MAX];
+
+string recursion(string x, string y, int n, int m){
+    if(n==0 || m==0){
+        return "";
+    }
+
+    if(x[n-1] == y[m-1]){
+        string str = recursion(x,y,n-1,m-1);
+        str.push_back(x[n-1]);
+        return str;
+    }
+
+    if(dp[n-1][m] > dp[n][m-1]){
+        return recursion(x,y,n-1,m);
+    }else{
+        return recursion(x,y,n,m-1);
+    }
+}
+
+vector<string> allLcs(string x, string y, int n, int m){
+    if(n==0 || m==0){
+        vector<string> v;
+        v.push_back("");
+        return v;
+    }
+
+    if(x[n-1] == y[m-1]){
+        vector<string> v = allLcs(x,y,n-1,m-1);
+        for(int i=0; i<v.size(); i++){
+            v[i].push_back(x[n-1]);
+        }
+        return v;
+    }
+
+    if(dp[n-1][m] > dp[n][m-1]){
+        return allLcs(x,y,n-1,m);
+    }
+    if(dp[n][m-1] > dp[n-1][m]){
+        return allLcs(x,y,n,m-1);
+    }
+
+    vector<string> top = allLcs(x,y,n-1,m);
+    vector<string> left = allLcs(x,y,n,m-1);
+    top.insert(top.end(), left.begin(), left.end());
+    return top;
+}
+
+int solve(string x, string y, int n, int m){
     for(int i=0; i<=n; i++){
         dp[i][0] = 0;
     }
@@ -21,34 +69,17 @@ int solve(string x, string y, int n, int m){
         }
     }
 
-    int i=n;
-    int j=m;
-    vector<char> v;
-    while(i>0 && j>0){
-        if(x[i-1] == y[j-1]){
-            v.push_back(x[i-1]);
-            i--;
-            j--;
-        }else{
-            if(dp[i][j-1]>dp[i-1][j]){
-                j--;
-            }else{
-                i--;
-            }
-        }
+    // cout<<recursion(x, y, n, m)<<endl;
+    vector<string> v = allLcs(x,y,n,m);
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<endl;
     }
-    reverse(v.begin(), v.end());
-    for(auto i=v.begin(); i!=v.end(); i++){
-        cout<<*i;
-    }
-
-    cout<<endl;
     return dp[n][m];
 }
 
 int main(){
-    string x = "AGGTAB";
-    string y = "GXTXAYB";
+    string x = "ABCBDAB";
+    string y = "BDCABA";
 
     int n = x.length();
     int m = y.length();
